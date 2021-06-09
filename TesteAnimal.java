2f719b584c02ba43449ac2d33ba249cdd097a55b
Michele Diones <85254821+nobodyknony@users.noreply.github.com>
2021-06-09 17:38:00 -0300

    Add files via upload

---
+++ TesteAnimal.java
@@ -0,0 +1,17 @@
+
+public class TesteAnimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cachorro bicho = new Cachorro (" Thor",2,4,"médio","","","preta","curta.");
+		Cavalo bicho2 =new Cavalo ("Spirit",5,15,"grande","","","branca ","curta e lisa");
+		Preguica bicho3=new Preguica ("Woli",3,"nao corre"," muito",4," medio,");
+				bicho.imprimirInfo();
+				bicho2.imprimirInfo();
+				bicho3.imprimirInfo();
+		
+		
+		
+		}
+
+}
