5a12756f00ec895e17275a8776fdd447da316c53
Michele Diones <85254821+nobodyknony@users.noreply.github.com>
2021-06-11 22:45:42 -0300

    Add files via upload
    atualizaçao

--- Animal1.java
+++ Animal1.java
@@ -1,38 +1,29 @@
+	package POO;
 /*1- Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e
 comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as
 características de forma que tudo o que for comum a todos os animais fique na classe
 Animal:*/
-	public class Animal1 {
-				
-			private String nome;
-			private int idade;
-			private int peso;
-			private String tamanho;
-			
-				public Animal1(String nome,int idade,int peso,String tamanho)
-				{
-					super();
-					this.nome = nome;
-					this.idade = idade;
-					this.peso=peso;
-					this.tamanho=tamanho;
-				}
-				
-				public String getNome() {
-					return nome;
-				}
-				
-				public int getIdade() {
-					return idade;
-				}
-				public int getPeso() {
-					return peso;
-				}
-				public String getTamanho() {
-					return tamanho;
-				}
-	}
+	public abstract class Animal1 {
+		
+		private String tipoAnimal;
+		
+		abstract public void Nome(String nomeAnimal);
+		abstract public void Idade(int idadeAnimal);
+		abstract public void Som(String somAnimal);
+		abstract public void Peso(int pesoAnimal);
+		abstract public void Tamanho(int tamanhoAnimal);
+		abstract public void Cor (String corAnimal);
+		abstract public void Pelagem (String peloAnimal);
+		public Animal1(String tipoAnimal)
+		{
+			this.tipoAnimal = tipoAnimal;
+		}
+		
+		public String getTipoAnimal() {
+			return tipoAnimal;
+		}
+		public void setTipoAnimal(String tipoAnimal) {
+			this.tipoAnimal = tipoAnimal;
+		}
 
-
-
-
+}
