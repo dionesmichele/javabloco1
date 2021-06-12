6dcfcf2fcbd3d995a8f13eb79611906eaa7fc131
Michele Diones <85254821+nobodyknony@users.noreply.github.com>
2021-06-11 22:47:40 -0300

    Add files via upload

---
+++ OutrosAnimais.java
@@ -0,0 +1,53 @@
+package POO;
+
+	public class OutrosAnimais extends Animal1{
+		
+		
+		public OutrosAnimais ()
+		{ super ("OutrosAnimais");
+		}
+
+		@Override
+		public void Nome(String nomeAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Idade(int idadeAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Som(String somAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Peso(int pesoAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Tamanho(int tamanhoAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Cor(String corAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void Pelagem(String peloAnimal) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+
+}
